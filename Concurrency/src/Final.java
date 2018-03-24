161cc17929288882a48c45f00c7331b49f29f05b
ericywl <midnight2903@gmail.com>
2018-03-25 01:08:36 +0800

    small edits to Final

--- Concurrency/src/Final.java
+++ Concurrency/src/Final.java
@@ -3,20 +3,27 @@ import java.math.BigInteger;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+/**
+ * FinalClientSocket is needed to run this properly
+ * Run this server class first before running FinalClientSocket
+ */
 public class Final {
+    /* Number of connections required before the server can input number for computation
+    aka. the number of FinalClientSocket instances to be run */
     private static final int numOfSockets = 2;
     private static volatile boolean isFound = false;
 
     public static void main(String[] args) {
         try {
             ServerSocket serverSocket = new ServerSocket(4321);
-            ClientThread[] cts = new ClientThread[numOfSockets];
+            ClientWorker[] cts = new ClientWorker[numOfSockets];
             int index = 0;
 
+            // Waiting for all the required connections
             System.out.println("Waiting for " + numOfSockets + " connections.");
             while (index < numOfSockets) {
                 Socket socket = serverSocket.accept();
-                cts[index] = new ClientThread(socket, index, numOfSockets);
+                cts[index] = new ClientWorker(socket, index, numOfSockets);
                 index++;
             }
             System.out.println("Finished establishing all " + numOfSockets + " connections.");
@@ -25,26 +32,30 @@ public class Final {
             BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
             String inputLine = null;
 
+            // Gets input from user and sends input to the clients
             while (inputLine == null) {
                 System.out.print("Enter a semi-prime number: ");
                 inputLine = stdIn.readLine();
-                if (inputLine != null && !inputLine.equalsIgnoreCase("") && checkLong(inputLine)) {
-                    for (ClientThread ct : cts) {
+                if (inputLine != null && checkLong(inputLine)) {
+                    for (ClientWorker ct : cts) {
                         ct.setNum(Long.valueOf(inputLine));
                     }
 
+                    // Wait for any client to respond
                     System.out.println("Awaiting client response...");
                     while (!isFound) {
                         // idle wait
                     }
 
-                    for (ClientThread ct : cts) {
+                    // Client responded, close all sockets and join the client workers
+                    for (ClientWorker ct : cts) {
                         ct.getSocket().close();
                         ct.join();
                     }
 
                 } else {
                     System.out.println("Invalid input.");
+                    inputLine = null;
                 }
             }
 
@@ -64,13 +75,13 @@ public class Final {
         }
     }
 
-    private static class ClientThread extends Thread {
+    private static class ClientWorker extends Thread {
         private Socket socket;
         private BigInteger n;
         private BigInteger init;
         private BigInteger stepSize;
 
-        public ClientThread(Socket socket, int index, int numOfThreads) {
+        public ClientWorker(Socket socket, int index, int numOfThreads) {
             this.socket = socket;
             this.init = BigInteger.valueOf(index + 2);
             this.stepSize = BigInteger.valueOf(numOfThreads);
