6fe0fb9dfbd990b96f7e142b1d7b11a455ce6c56
ericywl <midnight2903@gmail.com>
2018-03-29 19:25:01 +0800

    wat

---
+++ ThreadSafe/src/DiningPhilOrdered.java
@@ -0,0 +1,99 @@
+import java.util.Random;
+
+public class DiningPhilOrdered {
+    private static int numOfPhils = 5;
+
+    public static void main(String[] args) {
+        PhilosopherOrdered[] phils = new PhilosopherOrdered[numOfPhils];
+        ForkOrdered[] forks = new ForkOrdered[numOfPhils];
+
+        for (int i = 0; i < numOfPhils; i++) {
+            forks[i] = new ForkOrdered(i);
+        }
+
+        for (int i = 0; i < numOfPhils; i++) {
+            phils[i] = new PhilosopherOrdered(i, forks[i], forks[(i + numOfPhils - 1) % numOfPhils]);
+            phils[i].start();
+        }
+    }
+}
+
+class PhilosopherOrdered extends Thread {
+    private final int index;
+    private final ForkOrdered left;
+    private final ForkOrdered right;
+
+    public PhilosopherOrdered(int index, ForkOrdered left, ForkOrdered right) {
+        this.index = index;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void run() {
+        boolean orderCheck = left.getIndex() < right.getIndex();
+        ForkOrdered first = orderCheck ? left : right;
+        String firstStr = orderCheck ? "left" : "right";
+        ForkOrdered second = orderCheck ? right : left;
+        String secondStr = orderCheck ? "right" : "left";
+        Random randomGenerator = new Random();
+        try {
+            while (true) {
+                Thread.sleep(randomGenerator.nextInt(1000)); // not sleeping but thinking
+                System.out.println("Phil " + index + " finishes thinking.");
+                first.pickUp();
+                System.out.println("Phil " + index + " picks up " + firstStr + " fork.");
+                second.pickUp();
+                System.out.println("Phil " + index + " picks up " + secondStr + " fork.");
+
+                Thread.sleep(randomGenerator.nextInt(1000)); // eating
+                System.out.println("Phil " + index + " finishes eating.");
+                first.putDown();
+                System.out.println("Phil " + index + " picks up " + firstStr + " fork.");
+                second.putDown();
+                System.out.println("Phil " + index + " picks up " + secondStr + " fork.");
+
+            }
+        } catch (InterruptedException e) {
+            System.out.println("Don't disturb me while I am sleeping, well, thinking.");
+        }
+    }
+}
+
+class ForkOrdered {
+    private final int index;
+    private boolean isAvailable = true;
+
+    public ForkOrdered(int index) {
+        this.index = index;
+    }
+
+    public synchronized void pickUp() throws InterruptedException {
+        while (!isAvailable) {
+            wait();
+        }
+
+        isAvailable = false;
+        notifyAll();
+    }
+
+    public synchronized void putDown() throws InterruptedException {
+        while (isAvailable) {
+            wait();
+        }
+
+        isAvailable = true;
+        notifyAll();
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public String toString() {
+        if (isAvailable) {
+            return "ForkOrdered " + index + " is available.";
+        } else {
+            return "ForkOrdered " + index + " is NOT available.";
+        }
+    }
+}
---
+++ ThreadSafe/src/FirstExampleFixed.java
@@ -0,0 +1,33 @@
+import sun.rmi.transport.ObjectTable;
+
+import java.util.Vector;
+
+@SuppressWarnings("SynchronizationOnLocalVariableOrMethodParameter")
+public class FirstExampleFixed {
+    public static Object getLast(Vector list) {
+        synchronized (list) {
+            int lastIndex = list.size() - 1;
+            return list.get(lastIndex);
+        }
+    }
+
+    public static void deleteLast(Vector list) {
+        synchronized (list) {
+            int lastIndex = list.size() - 1;
+            list.remove(lastIndex);
+        }
+    }
+
+    public static boolean contains(Vector list, Object obj) {
+        synchronized (list) {
+            for (Object aList : list) {
+                if (aList.equals(obj)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+
+}
